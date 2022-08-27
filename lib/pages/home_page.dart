be2fdc7f3a00d9cdf5b4f2514310ba912220d75d
anteshtiwari <74955350+anteshtiwari@users.noreply.github.com>
2022-08-27 08:07:02 +0530

    now, day 1 of part 2 is completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,20 +3,17 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    int days = 30;
-    String name = "Indore";
-    const pi = 3.14;
-    bool isMale = true;
-    num temp = 30.0;
-
     return MaterialApp(
+      themeMode: ThemeMode.dark,
+      theme: ThemeData(
+          brightness: Brightness.light, primarySwatch: Colors.blueGrey),
       home: Scaffold(
         appBar: AppBar(
           title: Text("Catelog App"),
         ),
         body: Center(
           child: Container(
-            child: Text(' for $days fluttejmm in $name $pi $temp'),
+            child: Text('ANtesh Tiwari'),
           ),
         ),
         drawer: Drawer(),
